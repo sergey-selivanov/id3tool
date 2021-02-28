5493ab3f175e7ff10bcb336afbd7fb517ef72a25
sergeys <sergeys@zeppelin>
2021-02-28 10:54:36 +0200

    app skeleton

---
+++ src/main/java/module-info.java
@@ -0,0 +1,7 @@
+module org.svs.id3tool {
+
+    requires org.slf4j;
+    requires org.apache.logging.log4j;
+
+//    opens sssii.qtrac.gui to javafx.graphics;
+}
---
+++ src/main/java/org/svs/id3tool/Id3Tool.java
@@ -0,0 +1,14 @@
+package org.svs.id3tool;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+public class Id3Tool {
+
+    static Logger log = LoggerFactory.getLogger(Id3Tool.class);
+
+    public static void main(String[] args) {
+        log.info("hi");
+    }
+
+}
